35fe6a50a197d7f0ce6c68305262a71b676faf91
Karina khairnar <99542733+karinakhairnar@users.noreply.github.com>
2022-03-29 23:05:01 +0530

    login page

---
+++ mytraAppProject/login.js
@@ -0,0 +1,38 @@
+var loginData = [
+    {
+        //karina
+      number: '7083620943',
+      otp: '123',
+    },
+    // {
+    //     //bipin
+    //   number: '7871235082',
+    //   otp: '123',
+    // },
+    // {
+    //     //yash
+    //     number: '6395 320 028',
+    //     otp: '123',
+    // },
+    // {
+    //     //rakesh
+    //     number: '78946 82213',
+    //     otp: '123',
+    // },
+    // {
+    //     //shakti
+    //     number: '77424 14814',
+    //     otp: '123',
+    // },
+    // {
+    //     //shakti
+    //     number: '77424 14814',
+    //     otp: '123',
+    // },
+    // {
+    //     //kiran
+    //     number: '80896 63158',
+    //     otp: '123',
+   // }
+  ];
+  localStorage.setItem('loginData', JSON.stringify(loginData));
